f35e3085150b70a23fc7791037e8eb2fec7ca6ef
Eric G. E. Nilsen <e.g.e.nilsen@stv.uio.no>
2022-09-21 12:05:16 +0200

    Ferdig sem 2

--- Prossesere_Data.R
+++ Prossesere_Data.R
@@ -1,11 +1,15 @@
 library(tidyverse)
 library(quanteda)
-library(janeaustenr)
 library(tidytext)
 library(ggdark)
 library(quanteda.textplots)
 
-nrk_art
+
+githubURL <- ("https://github.com/egen97/2022seminar/raw/master/sma_filer/nrk_artikler.rds")
+
+nrk_artikler <- readRDS(url(githubURL, method="libcurl"))
+
+
 
 nrk_df <- as.data.frame(do.call(rbind, nrk_artikler))
 
@@ -16,34 +20,11 @@ bag_of_words <- nrk_df %>%
   mutate(text = str_split(text,"\\s")) %>%
   unlist()
 
-set.seed(984301)
 
-cat(bag_of_words[sample(1:length(bag_of_words))])
+cat(bag_of_words[sample(1:100)])
 
 cat(nrk_df$text[which(str_detect(nrk_df$text, "EU"))])
 
-
-original_books <- austen_books() %>%
-  group_by(book) %>%
-  mutate(line = row_number()) %>%
-  ungroup()
-
-
-tidy_books <- original_books %>%
-  unnest_tokens(word, text) %>% 
-  count(word) %>% 
-  arrange(desc(n))
-
-tidy_books %>% head(300) %>% 
-  ggplot(., aes(x = 1:300, y = n)) +
-  geom_point() +
-  geom_line(aes(group = 1)) +
-  scale_y_continuous(trans = "log") +
-  scale_x_continuous(trans = "log") +
-  geom_smooth(method = "lm", se = FALSE) +
-  ggrepel::geom_label_repel(aes(label = word)) +
-  ggdark::dark_theme_classic() +
-  labs(x = "Rangering (log)", y = "Frekvens (log)", title = "Zipf's lov illustrasjon")
 
 
 
@@ -70,7 +51,7 @@ nrk_tokens %>%
 
 nrk_tokens %>% 
   count(token) %>% 
-  filter(token %in% quanteda::stopwords("no") == FALSE) %>% 
+  filter(!(token %in% quanteda::stopwords("no"))) %>% 
   slice_max(order_by = n,
             n = 2,
             with_ties = FALSE)
@@ -104,6 +85,26 @@ nrk_tokens %>%
             with_ties = FALSE)
 
 
+nrk_tokens %>%
+  #filter(!(token %in% idf_stop$token)) %>% 
+  filter(!str_detect(token, "\\d")) %>% 
+  count(token) %>%
+  arrange(desc(n)) %>% 
+  head(300) %>% 
+  ggplot(aes(x = 1:300, y = n)) +
+  geom_point() +
+  geom_line(aes(group = 1)) +
+  scale_y_continuous(trans = "log") +
+  scale_x_continuous(trans = "log") +
+  geom_smooth(method = "lm", se = FALSE) +
+  ggrepel::geom_label_repel(aes(label = token)) +
+  ggdark::dark_theme_classic() +
+  labs(x = "Rangering (log)", y = "Frekvens (log)", title = "Zipf's lov illustrasjon")
+
+
+
+
+
 
 ## Lage en data-feature-matrix
 
@@ -122,10 +123,73 @@ nrk_tokens <- tokens(nrk_corpus,
 
 nrk_tokens <- tokens_tolower(nrk_tokens)
 
-nrk_tokens <- tokens_remove(nrk_tokens, pattern = stopwords('no'))
+
+
+
+
+nrk_tokens <- tokens_remove(nrk_tokens, pattern = c(stopwords('no')))
+
+nrk_tokens <- tokens_remove(nrk_tokens, pattern = c(stopwords('no'), "amp"))
 
 nrk_dfm <- dfm(nrk_tokens)
 
-textplot_network(nrk_dfm, min_freq = 1)
+
 textplot_wordcloud(nrk_dfm)
-?textplot_xray()
+
+
+
+
+
+
+kwic(nrk_tokens, "amp")
+
+nrk_kwik <- kwic(nrk_tokens, "EU")
+
+nrk_tokens <- tokens_remove(nrk_tokens, pattern = c(stopwords('no'), "amp"))
+
+textplot_xray(kwic(nrk_tokens, "nrk")) +
+  ggthemes::theme_fivethirtyeight() + 
+  aes(color = keyword) + 
+  scale_color_manual(values = "red")
+
+textplot_network(nrk_dfm)
+
+spr_time <- stortingscrape::get_question_hour(300)
+
+spr_time <- readRDS("sma_filer/spr_time_300.rds")
+spr_time <- spr_time$question_time
+
+spr_time_corpus <- corpus(spr_time, docid_field = "question_id", text_field = "question_text")
+
+
+spr_time_tokens <- tokens(spr_time_corpus,
+                     remove_numbers = TRUE,
+                     remove_punct = TRUE,
+                     remove_symbols = TRUE,
+                     remove_separators = TRUE,
+                     remove_url = TRUE,
+                     verbose = TRUE
+)
+
+
+spr_time_tokens <- tokens_tolower(spr_time_tokens)
+
+spr_time_tokens <- tokens_remove(spr_time_tokens, pattern = stopwords('no'))
+
+spr_time_dfm <- dfm(spr_time_tokens)
+
+textplot_xray(kwic(spr_time_tokens, "regjering*")) + 
+  ggthemes::theme_fivethirtyeight() + 
+  aes(color = keyword) + 
+  scale_color_manual(values = "red")
+
+textplot_network(spr_time_dfm, min_freq = .9)
+
+
+?tokens
+spr_time_stem <- dfm_wordstem(spr_time_dfm, language = "no")
+textplot_wordcloud(spr_time_stem)
+textplot_network(spr_time_stem, min_freq = .9)
+
+
+
